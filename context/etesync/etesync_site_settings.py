8d78e03fd97ac25935bce375cfe59da1d399463c
Edyr Costa <edyrcosta12@gmail.com>
2020-08-24 19:35:18 -0300

    Added support for PostgreSQL.

--- context/etesync/etesync_site_settings.py
+++ context/etesync/etesync_site_settings.py
@@ -13,3 +13,15 @@ USE_TZ = os.environ.get('USE_TZ', False)
 TIME_ZONE = os.environ.get('TIME_ZONE', 'UTC')
 
 SECRET_FILE = os.environ.get('SECRET_FILE')
+
+if os.environ.get('USE_POSTGRES', '') == '1':
+  DATABASES = {
+    'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.environ.get('DATABASE_NAME', 'etesync'),
+      'USER': os.environ.get('DATABASE_USER', 'etesync'),
+      'PASSWORD': os.environ.get('DATABASE_PASSWORD', 'etesync'),
+      'HOST': os.environ.get('DATABASE_HOST', ''),
+      'PORT': os.environ.get('DATABASE_PORT', '5432')
+    }
+  }
